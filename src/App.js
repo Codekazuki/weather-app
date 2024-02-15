9399bbb698e040df444f4de560fa4fa867a81298
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-02-15 21:29:51 +0100

    getting better

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import { Button } from "@mui/material";
+import { Button, Card, Grid, Typography } from "@mui/material";
 import "./App.css";
 
 function App() {
   return (
     <div className='App'>
-      <Button variant='contained'>Lets go</Button>
+      <Card>
+        <Grid container spacing={2}>
+          <Grid item xs={8}>
+            <Button variant='contained'>Lets go</Button>
+          </Grid>
+          <Grid item xs={4}>
+            <Typography>Some Countries</Typography>
+          </Grid>
+          <Grid item xs={4}>
+            <Typography>Weather</Typography>
+          </Grid>
+          <Grid item xs={8}>
+            <Typography> Humidity </Typography>
+          </Grid>
+        </Grid>
+      </Card>
     </div>
   );
 }
