12a0b491fcb845f023ce7ec64b7c3e231ca1b589
Ayodele Agbowuro <ayodele.agbowuro@prunedge.com>
2024-02-15 21:49:38 +0100

    getting better

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import { Button, Card, Grid, Typography } from "@mui/material";
+import { Button, Card, Typography } from "@mui/material";
+
+import Grid from "@mui/material/Unstable_Grid2";
+
 import "./App.css";
 
 function App() {
   return (
     <div className='App'>
-      <Card>
-        <Grid container spacing={2}>
-          <Grid item xs={8}>
-            <Button variant='contained'>Lets go</Button>
+      <Card sx={{ backgroundColor: "red" }}>
+        <Grid
+          container
+          spacing={{ xs: 2, md: 3 }}
+          columns={{ xs: 4, sm: 8, md: 12 }}
+        >
+          <Grid xs={8}>
+            <Typography>xs=8</Typography>
           </Grid>
-          <Grid item xs={4}>
-            <Typography>Some Countries</Typography>
+          <Grid xs={4}>
+            <Button>xs=4</Button>
           </Grid>
-          <Grid item xs={4}>
-            <Typography>Weather</Typography>
+          <Grid xs={4}>
+            <Typography>xs=4</Typography>
           </Grid>
-          <Grid item xs={8}>
-            <Typography> Humidity </Typography>
+          <Grid xs={8}>
+            <Button>xs=8</Button>
           </Grid>
         </Grid>
       </Card>
